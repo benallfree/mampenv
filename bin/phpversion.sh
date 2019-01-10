43066842b0a3310e5ecce42f81ba50a20b8d19da
Ben Allfree <ben@benallfree.com>
2019-01-10 01:46:16 -0800

    PHP ini config support

--- bin/phpversion.sh
+++ bin/phpversion.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
 function upsearch () {
-  slashes=${PWD//[^\/]/}
-  directory="$PWD"
-  for (( n=${#slashes}; n>0; --n ))
-  do
-    test -e "${directory}"/"$1" && echo "$directory"/"$1" && return 
-    directory="$directory"/..
-  done
+    slashes=${PWD//[^\/]/}
+    directory="$PWD"
+    for (( n=${#slashes}; n>0; --n ))
+    do
+        test -e "${directory}"/"$1" && echo "$directory"/"$1" && return
+        directory="$directory"/..
+    done
 }
 
 value=$(upsearch ".phpversion")
-if [[ -z "$value" ]]; then 
-  echo "A .phpversion file must be defined. http://github.com/benallfree/mampenv"
-  exit 1
-else 
-  version=$(cat "${value}")
+if [[ -z "$value" ]]; then
+    echo "A .phpversion file must be defined. http://github.com/benallfree/mampenv"
+    exit 1
+else
+    version=$(cat "${value}")
 fi
 php="/Applications/MAMP/bin/php/php${version}/bin/php"
 
 if [ ! -e "$php" ]; then
-  echo "PHP $version specified in .phpversion was not found ($php). http://github.com/benallfree/mampenv"
-  exit 1
+    echo "PHP $version specified in .phpversion was not found ($php). http://github.com/benallfree/mampenv"
+    exit 1
 fi
+
+conf="/Library/Application Support/appsolute/MAMP PRO/conf/php${version}.ini"
